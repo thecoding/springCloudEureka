4a699e05114884efe7161bfe3db05b89b6e39fec
Mirko <zhouchao180@gmail.com>
2019-10-06 16:39:23 +0800

    update client order package

--- order-api/src/main/java/com/order/controller/OrderInfoController.java
+++ order-api/src/main/java/com/order/controller/OrderInfoController.java
@@ -1,4 +1,4 @@
-package com.eureka.controller;
+package com.order.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
