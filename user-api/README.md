54e868b88877b6daf30c5fcee1e09be15612d2ab
Mirko <zhouchao180@gmail.com>
2019-10-10 02:56:24 +0800

    add descript

---
+++ user-api/README.md
@@ -0,0 +1,7 @@
+# client user api
+
+####完成功能
+
+  1、日志打印，在com.user.aop包下，通过aop实现请求入参打印\
+  2、集成了Feign，代码看orderInterface，feign-hystrix默认没有开启，也有测试案例，还有feign-hystrix熔断，interface中的fallback\
+  3、ribbon服务调用的负载均衡
--- user-api/src/main/java/com/user/controller/UserToOrderClient.java
+++ user-api/src/main/java/com/user/controller/UserToOrderClient.java
@@ -2,6 +2,7 @@ package com.user.controller;
 
 import com.user.service.OrderInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
